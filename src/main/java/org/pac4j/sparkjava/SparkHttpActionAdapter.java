41e7c5c77adb658aa095b45395b7e8d17e52111e
Jérôme LELEU <leleuj@gmail.com>
2022-09-09 09:18:56 +0200

    clean after pac4j v5.5

--- src/main/java/org/pac4j/sparkjava/SparkHttpActionAdapter.java
+++ src/main/java/org/pac4j/sparkjava/SparkHttpActionAdapter.java
@@ -1,6 +1,5 @@
 package org.pac4j.sparkjava;
 
-import org.pac4j.core.context.HttpConstants;
 import org.pac4j.core.context.WebContext;
 import org.pac4j.core.exception.TechnicalException;
 import org.pac4j.core.exception.http.HttpAction;
@@ -9,7 +8,6 @@ import org.pac4j.core.exception.http.WithLocationAction;
 import org.pac4j.core.http.adapter.HttpActionAdapter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import spark.Service;
 import spark.Spark;
 
@@ -44,10 +42,6 @@ public class SparkHttpActionAdapter implements HttpActionAdapter {
 				stop(code, ((WithContentAction) action).getContent());
 			} else if (action instanceof WithLocationAction) {
 				((SparkWebContext) context).getSparkResponse().redirect(((WithLocationAction) action).getLocation(), code);
-			} else if (code == HttpConstants.UNAUTHORIZED) {
-				stop(HttpConstants.UNAUTHORIZED, "authentication required");
-			} else if (code == HttpConstants.FORBIDDEN) {
-				stop(HttpConstants.FORBIDDEN, "forbidden");
 			} else {
 				stop(code, "");
 			}
